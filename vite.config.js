6cb26a2fae18e970672b97ab853c62df99b20216
Namide <damien@doussaud.fr>
2023-12-26 23:28:08 +0100

    Lint files

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import glsl from 'vite-plugin-glsl';
+import { defineConfig } from "vite";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  base: process.env.BASE || '/',
+  base: process.env.BASE || "/",
   plugins: [glsl()],
   build: {
     rollupOptions: {
       output: {
         manualChunks: (id) => {
           console.log(id);
-          if (id.includes('three')) {
-            return 'render';
+          if (id.includes("three")) {
+            return "render";
           }
 
-          return 'index';
+          return "index";
         },
       },
     },
